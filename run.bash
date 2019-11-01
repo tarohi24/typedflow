f7de4ed2aa8854e68e96d1b1bdeadf52be094b18
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-01 05:14:05 +0000

    mod tests

--- run.bash
+++ run.bash
@@ -2,4 +2,4 @@
 
 COMPOSE_FILE="docker-compose.yaml"
 CONTAINER="base"
-docker-compose -f ${COMPOSE_FILE} run --rm ${CONTAINER} make${@:2}
+docker-compose -f ${COMPOSE_FILE} run --rm ${CONTAINER} make${@:1}
