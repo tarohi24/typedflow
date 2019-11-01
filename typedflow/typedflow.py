44d2fb601d7c294ddf012c5a398c70cf1836e3c7
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-01 13:21:08 +0000

    flake8

--- typedflow/typedflow.py
+++ typedflow/typedflow.py
@@ -1,6 +1,14 @@
 from __future__ import annotations
-from dataclasses import dataclass, field
-from typing import Callable, Generic, Generator, Iterable, Iterator, List, Type, TypeVar
+from dataclasses import dataclass
+from typing import (
+    Callable,
+    Generic,
+    Generator,
+    Iterable,
+    Iterator,
+    List,
+    TypeVar
+)
 
 
 T = TypeVar('T')  # serializable
