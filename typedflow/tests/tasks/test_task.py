916fb856a9d2d9a969627c6cfb9579ded9ebc2c6
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-05 11:59:42 +0000

    make test_task executable

--- typedflow/tests/tasks/test_task.py
+++ typedflow/tests/tasks/test_task.py
@@ -4,26 +4,26 @@ import pytest
 
 from typedflow.arguments import TaskArguments
 from typedflow.batch import Batch
-from typedflow.defaults import IntArgument, StringArguemnt
+from typedflow.defaults import IntArgument, StrArgument
 from typedflow.tasks import Task
 
 
 @pytest.fixture
 def task() -> Task:
-    def count_chars(s: StringArguemnt) -> IntArgument:
+    def count_chars(s: StrArgument) -> IntArgument:
         return IntArgument(len(s.value))
-    task: Task = Task[StringArguemnt, IntArgument](func=count_chars)
+    task: Task = Task[StrArgument, IntArgument](func=count_chars)
     return task
 
 
-@pytest.fixture
-def gen_batches() -> Generator[Batch[StringArguemnt], None, None]:
-    strs: List[StringArguemnt] = [StringArguemnt(s) for s in ['hi', 'hello']]
+def gen_batches() -> Generator[Batch[StrArgument], None, None]:
+    strs: List[StrArgument] = [StrArgument(s) for s in ['hi', 'hello']]
     for i in range(2):
         yield Batch(batch_id=i, data=strs)
 
 
-def test_task(task, gen_batches):
-    mat: Batch[StringArguemnt] = next(gen_batches)
-    prod: Batch[IntArgument] = task.process(batch)
-    assert prod.data == [2, 5]
+def test_task(task):
+    gen: Generator[Batch[StrArgument], None, None] = gen_batches()
+    mat: Batch[StrArgument] = next(gen)
+    prod: Batch[IntArgument] = task.process(mat)
+    assert [item.value for item in prod.data] == [2, 5]
