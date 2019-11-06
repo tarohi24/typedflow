66c6f1fe497d0f0ab30d7683ed594a6c29fad322
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-06 08:37:35 +0000

    add provider tests #35

---
+++ typedflow/tests/nodes/test_loader_node.py
@@ -0,0 +1,31 @@
+from typing import List
+
+import pytest
+
+from typedflow.nodes import LoaderNode
+from typedflow.tasks import DataLoader
+
+
+@pytest.fixture
+def loader_node() -> LoaderNode[str]:
+    lst: List[str] = ['hi', 'hello', 'konnichiwa']
+    loader: DataLoader[str] = DataLoader(gen=lst, batch_size=2)
+    node: LoaderNode[str] = LoaderNode(loader=loader)
+    return node
+
+def test_get_or_produce_batch(loader_node):
+    node = loader_node
+    node.add_succ()
+    assert node.get_or_produce_batch(0).data == ['hi', 'hello']
+    assert node.get_or_produce_batch(1).data == ['konnichiwa']
+
+
+def test_cache(loader_node):
+    node = loader_node
+    node.add_succ()
+    node.add_succ()
+    batch = node.get_or_produce_batch(0)
+    assert batch.data == ['hi', 'hello']
+    assert node.cache_table.cache_table[0].value.data == ['hi', 'hello']
+    assert node.get_or_produce_batch(0).data == ['hi', 'hello']
+    assert 0 not in node.cache_table.cache_table
--- typedflow/tests/nodes/test_provider.py
+++ typedflow/tests/nodes/test_provider.py
@@ -8,7 +8,7 @@ from typedflow.tasks import DataLoader
 
 @pytest.fixture
 def loader_node() -> LoaderNode[str]:
-    lst: List[str] = ['hi', 'hello']
+    lst: List[str] = ['hi', 'hello', 'konnichiwa']
     loader: DataLoader[str] = DataLoader(gen=lst, batch_size=2)
     node: LoaderNode[str] = LoaderNode(loader=loader)
     return node
@@ -18,3 +18,12 @@ def test_init(loader_node):
     node = loader_node
     assert node.succ_count == 0
     assert len(node.cache_table) == 0
+
+
+def test_add_succ(loader_node):
+    node = loader_node
+    node.add_succ()
+    node.add_succ()
+    assert node.succ_count == 2
+    assert len(node.cache_table) == 0
+    assert node.cache_table.life == 2
