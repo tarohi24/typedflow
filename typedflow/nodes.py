3df4dad8d62461e2a56cad3221915af58d69c47e
Wataru Hirota <nobuyoshi2426@gmail.com>
2019-11-06 09:42:00 +0000

    add tests for cunsumers #41

--- typedflow/nodes.py
+++ typedflow/nodes.py
@@ -20,7 +20,7 @@ from typedflow.tasks import (
 from typedflow.types import T, K
 
 
-__all__ = ['DumpNode']
+__all__ = ['LoaderNode', 'TaskNode', 'DumpNode']
 
 
 logger = logging.getLogger(__file__)
@@ -32,11 +32,11 @@ class ProviderNode(Generic[K]):
     """
     LoaderNode or TaskNode
     """
-    succ_count: int = field(init=False)
     cache_table: CacheTable[int, Batch[K]] = field(init=False)
+    _succ_count: int = field(init=False)
 
     def __post_init__(self):
-        self.succ_count: int = 0
+        self._succ_count: int = 0
         self.cache_table: CacheTable[int, Batch[K]]\
             = CacheTable[int, Batch[K]](life=0)
 
@@ -45,7 +45,7 @@ class ProviderNode(Generic[K]):
         ...
 
     def add_succ(self):
-        self.succ_count += 1
+        self._succ_count += 1
         self.cache_table.life += 1
 
 
@@ -56,36 +56,37 @@ class ConsumerNode(Generic[T]):
     Note: this is not defined as a dataclass due to the inheritance problem
     see: https://stackoverflow.com/questions/51575931/class-inheritance-in-python-3-7-dataclasses  # noqa
     """
-    arg_type: Type[T] = field(init=False)  # must be same as T
+    arg_type: Type[T]  # must be same as T
     precs: Dict[str, ProviderNode[T]] = field(init=False)
 
-    def __post_init__(self, arg_type: Type[T]):
-        self.arg_type: Type[T] = arg_type  # must be same as T
+    def __post_init__(self):
         self.precs: Dict[str, ProviderNode[T]] = dict()
 
     def set_upstream_node(self,
                           key: str,
                           node: ProviderNode[T]) -> None:
+        assert key not in self.precs
         self.precs[key] = node
-        node.succ_count += 1
+        node.add_succ()
 
     def _merge_batches(self,
                        materials: Dict[str, Batch]) -> Batch[TD]:
         """
         TD is the same class as self.arg_type
         """
+        batch_id: int = next(iter(materials.values())).batch_id
         material_data: Dict[str, List] = {key: batch.data
                                           for key, batch
-                                          in self.materials.items()}
+                                          in materials.items()}
         keys, values = list(zip(*material_data.items()))
         data: List[TD] = [
             self.arg_type({key: list(val) for key in keys})
             for val in zip(*values)
         ]
-        batch = Batch(batch_id=self.batch_id, data=data)
+        batch = Batch(batch_id=batch_id, data=data)
         return batch
 
-    async def acecpt(self,
+    async def accept(self,
                      batch_id: int) -> Batch[T]:
         """
         merge all the arguments items into an instance of T (=arg_type)
@@ -95,7 +96,6 @@ class ConsumerNode(Generic[T]):
             product: Batch[T] = await prec.get_or_produce_batch(batch_id=batch_id)
             return product
         else:
-            assert issubclass(self.arg_type, TypedDict)
             materials: Dict[str, Batch] = {
                 key: await prec.get_or_produce_batch(batch_id=batch_id)
                 for key, prec in self.precs.items()
@@ -113,8 +113,8 @@ class LoaderNode(ProviderNode[K]):
         super().__post_init__()
         self.itr: Iterator[K] = iter(self.loader.load())
 
-    def get_or_produce_batch(self,
-                             batch_id: int) -> Batch[K]:
+    async def get_or_produce_batch(self,
+                                   batch_id: int) -> Batch[K]:
         try:
             return self.cache_table.get(batch_id)
         except KeyError:
